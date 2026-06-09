dd9249a3e86023ede0e5d1374e3e11307dfd6d13
Kartik Kasaudhan <kartikgupta3105@gmail.com>
2026-06-09 18:53:39 +0530

    Add files via upload

---
+++ src/main/controller/PublicController.java
@@ -0,0 +1,22 @@
+package com.kartik.journalApp.controller;
+
+import com.kartik.journalApp.entity.entity.User;
+import com.kartik.journalApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/public")
+public class PublicController {
+    @Autowired
+    private UserService userService;
+    @GetMapping("/health-check")
+    public String healthCheck(){
+        return "OK";
+    }
+
+    @PostMapping("/create-user")
+    public void createUser(@RequestBody User user){
+        userService.saveNewUser(user);
+    }
+}
