76e1c8b54f734c256d4f265abebc3b89da84e788
Kartik Kasaudhan <kartikgupta3105@gmail.com>
2026-06-11 20:08:42 +0530

    Add files via upload

--- src/main/repository/UserRepository.java
+++ src/main/repository/UserRepository.java
@@ -1,1 +1,12 @@
+package com.kartik.journalApp.repository;
+import com.kartik.journalApp.entity.JournalEntry;
+import com.kartik.journalApp.entity.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
+public interface UserRepository extends MongoRepository<User, ObjectId>{
+    User findByUserName(String username);
+
+    void deleteByUserName(String username);
+
+}
