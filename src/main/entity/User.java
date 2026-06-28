7fc079b30b270c84096705ae6a0e75b9535ee8e8
Kartik Kasaudhan <kartikgupta3105@gmail.com>
2026-06-28 20:05:25 +0530

    Add files via upload

--- src/main/entity/User.java
+++ src/main/entity/User.java
@@ -1,1 +1,29 @@
+package com.kartik.journalApp.entity.entity;
 
+import com.kartik.journalApp.entity.JournalEntry;
+import lombok.Data;
+import lombok.NonNull;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collection = "users")
+@Data     // use @Getter @Setter or you can use @Data that contains all the annotations
+public class User {
+    @Id
+    private ObjectId id;
+
+    @Indexed(unique = true)
+    @NonNull
+    private String userName;
+    @NonNull
+    private String password;
+    @DBRef
+    private List<JournalEntry> journalEntries = new ArrayList<>();
+    private List<String> roles;
+}
